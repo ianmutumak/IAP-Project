bf4e67eaaa55028ec032b1da1509cb797593a4c7
Muyesto <143603085+Muyesto@users.noreply.github.com>
2023-11-16 21:34:10 +0300

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use app\Http\Controllers\regmanager;
+use App\Http\Controllers\MainController;
 
 /*
 |--------------------------------------------------------------------------
@@ -9,23 +9,50 @@ use app\Http\Controllers\regmanager;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
 Route::get('/', function () {
-    return view('welcome');
-})->name('welcome');
+    return view('landing');
+});
+Route::get('/login', [MainController::class, 'login']);
+Route::post('/auth/check', [MainController::class, 'check'])->name('auth.check');
+Route::post('/auth/save', [MainController::class, 'save'])->name('auth.save');
 
-Route::get('/signin', function(){
-    return view('signin');
+Route::get('/enroll', function () {
+    return view('enroll');
 });
-//Route::post('/signin',[regmanager::class, 'signinpost'])->name('signin.post');
-Route::get('/signup', function(){
-    return view('signup');
+
+
+Route::get('/student', [MainController::class, 'students'])->name('student');
+Route::get('/register_units', [MainController::class, 'register_units']);
+
+Route::get('/edit-student/{id}', [MainController::class, 'edit']);
+Route::post('/update-student/{id}', [MainController::class, 'update_student']);
+
+
+Route::get('/notices', [MainController::class, 'notices']);
+Route::get('/noticesteacher', [MainController::class, 'notices_teacher']);
+Route::get('/coursework', function () {
+    return view('STC/coursework');
 });
-Route::get('/home1', function(){
-    return view('home1');
-});
-//Route::post('/signup',[regmanager::class, 'signuppost'])->name('signup.post');
+Route::get('/teacher', [MainController::class, 'lec'])->name('teacher');
+Route::get('/admin', [MainController::class, 'admin']);
+Route::get('/auth/logout', [MainController::class, 'logout'])->name('auth.logout');
+Route::get('/unit/{id}', [MainController::class, 'unit']);
+
+
+//functionality routes
+Route::get('delete-student/{id}', [MainController::class, 'delete_student']);
+Route::get('delete-unit/{id}', [MainController::class, 'delete_unit']);
+Route::post('/auth/add-unit', [MainController::class, 'add_unit'])->name('auth.add-unit');
+Route::post('/auth/add-staff', [MainController::class, 'add_staff'])->name('auth.add-staff');
+Route::post('/auth/add-notice', [MainController::class, 'add_notice'])->name('auth.add-notice');
+// Route::post('/auth/add-cwork', [MainController::class, 'add_cwork'])->name('auth.add-cwork');
+Route::post('/reg_unit/{id}', [MainController::class, 'reg_unit']);
+Route::get('/course/{id}', [MainController::class, 'course']);
+Route::post('/add-work/{id}', [MainController::class, 'add_cwork']);
+Route::get('delete-work/{id}', [MainController::class, 'delete_work']);
+Route::get('delete-staff/{id}', [MainController::class, 'delete_staff']);
